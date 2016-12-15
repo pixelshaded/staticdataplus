e06dfc4842f3958e0f37787c2cf3176208091871
Alexander Fisher <alexander.fisher@ignitionone.com>
2016-12-15 11:34:27 -0500

    Change name of edit button to add button. Change title of form to creating connection strings. Minor stuff.

--- Win/ConnectionStringCreator.Designer.cs
+++ Win/ConnectionStringCreator.Designer.cs
@@ -49,26 +49,29 @@
             // 
             this.serverNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.serverNameTextBox.Location = new System.Drawing.Point(127, 22);
+            this.serverNameTextBox.Location = new System.Drawing.Point(169, 27);
+            this.serverNameTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.serverNameTextBox.Name = "serverNameTextBox";
-            this.serverNameTextBox.Size = new System.Drawing.Size(240, 20);
+            this.serverNameTextBox.Size = new System.Drawing.Size(319, 22);
             this.serverNameTextBox.TabIndex = 0;
             // 
             // serverNameLabel
             // 
             this.serverNameLabel.AutoSize = true;
-            this.serverNameLabel.Location = new System.Drawing.Point(22, 25);
+            this.serverNameLabel.Location = new System.Drawing.Point(29, 31);
+            this.serverNameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.serverNameLabel.Name = "serverNameLabel";
-            this.serverNameLabel.Size = new System.Drawing.Size(72, 13);
+            this.serverNameLabel.Size = new System.Drawing.Size(91, 16);
             this.serverNameLabel.TabIndex = 1;
             this.serverNameLabel.Text = "Server Name:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 64);
+            this.label1.Location = new System.Drawing.Point(29, 79);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.Size = new System.Drawing.Size(94, 16);
             this.label1.TabIndex = 3;
             this.label1.Text = "Authentication:";
             // 
@@ -78,18 +81,20 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.authenticationComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.authenticationComboBox.FormattingEnabled = true;
-            this.authenticationComboBox.Location = new System.Drawing.Point(127, 61);
+            this.authenticationComboBox.Location = new System.Drawing.Point(169, 75);
+            this.authenticationComboBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.authenticationComboBox.Name = "authenticationComboBox";
-            this.authenticationComboBox.Size = new System.Drawing.Size(240, 21);
+            this.authenticationComboBox.Size = new System.Drawing.Size(319, 24);
             this.authenticationComboBox.TabIndex = 4;
             this.authenticationComboBox.SelectedIndexChanged += new System.EventHandler(this.authenticationComboBox_SelectedIndexChanged);
             // 
             // userNameLabel
             // 
             this.userNameLabel.AutoSize = true;
-            this.userNameLabel.Location = new System.Drawing.Point(22, 106);
+            this.userNameLabel.Location = new System.Drawing.Point(29, 130);
+            this.userNameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.userNameLabel.Name = "userNameLabel";
-            this.userNameLabel.Size = new System.Drawing.Size(63, 13);
+            this.userNameLabel.Size = new System.Drawing.Size(80, 16);
             this.userNameLabel.TabIndex = 6;
             this.userNameLabel.Text = "User Name:";
             // 
@@ -97,17 +102,19 @@
             // 
             this.userNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.userNameTextBox.Location = new System.Drawing.Point(127, 103);
+            this.userNameTextBox.Location = new System.Drawing.Point(169, 127);
+            this.userNameTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.userNameTextBox.Name = "userNameTextBox";
-            this.userNameTextBox.Size = new System.Drawing.Size(240, 20);
+            this.userNameTextBox.Size = new System.Drawing.Size(319, 22);
             this.userNameTextBox.TabIndex = 5;
             // 
             // passwordLabel
             // 
             this.passwordLabel.AutoSize = true;
-            this.passwordLabel.Location = new System.Drawing.Point(22, 144);
+            this.passwordLabel.Location = new System.Drawing.Point(29, 177);
+            this.passwordLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.passwordLabel.Name = "passwordLabel";
-            this.passwordLabel.Size = new System.Drawing.Size(56, 13);
+            this.passwordLabel.Size = new System.Drawing.Size(71, 16);
             this.passwordLabel.TabIndex = 8;
             this.passwordLabel.Text = "Password:";
             // 
@@ -115,10 +122,11 @@
             // 
             this.passwordTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.passwordTextBox.Location = new System.Drawing.Point(127, 141);
+            this.passwordTextBox.Location = new System.Drawing.Point(169, 174);
+            this.passwordTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.passwordTextBox.Name = "passwordTextBox";
             this.passwordTextBox.PasswordChar = '*';
-            this.passwordTextBox.Size = new System.Drawing.Size(240, 20);
+            this.passwordTextBox.Size = new System.Drawing.Size(319, 22);
             this.passwordTextBox.TabIndex = 7;
             // 
             // databaseNameComboBox
@@ -127,27 +135,30 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.databaseNameComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.databaseNameComboBox.FormattingEnabled = true;
-            this.databaseNameComboBox.Location = new System.Drawing.Point(127, 214);
+            this.databaseNameComboBox.Location = new System.Drawing.Point(169, 263);
+            this.databaseNameComboBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.databaseNameComboBox.Name = "databaseNameComboBox";
-            this.databaseNameComboBox.Size = new System.Drawing.Size(240, 21);
+            this.databaseNameComboBox.Size = new System.Drawing.Size(319, 24);
             this.databaseNameComboBox.TabIndex = 9;
             this.databaseNameComboBox.SelectedIndexChanged += new System.EventHandler(this.databaseNameComboBox_SelectedIndexChanged);
             // 
             // databaseNameLabel
             // 
             this.databaseNameLabel.AutoSize = true;
-            this.databaseNameLabel.Location = new System.Drawing.Point(22, 217);
+            this.databaseNameLabel.Location = new System.Drawing.Point(29, 267);
+            this.databaseNameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.databaseNameLabel.Name = "databaseNameLabel";
-            this.databaseNameLabel.Size = new System.Drawing.Size(87, 13);
+            this.databaseNameLabel.Size = new System.Drawing.Size(111, 16);
             this.databaseNameLabel.TabIndex = 10;
             this.databaseNameLabel.Text = "Database Name:";
             // 
             // okButton
             // 
             this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.okButton.Location = new System.Drawing.Point(211, 296);
+            this.okButton.Location = new System.Drawing.Point(281, 364);
+            this.okButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.okButton.Name = "okButton";
-            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.Size = new System.Drawing.Size(100, 28);
             this.okButton.TabIndex = 11;
             this.okButton.Text = "Add";
             this.okButton.UseVisualStyleBackColor = true;
@@ -156,9 +167,10 @@
             // cancelButton
             // 
             this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.cancelButton.Location = new System.Drawing.Point(292, 296);
+            this.cancelButton.Location = new System.Drawing.Point(389, 364);
+            this.cancelButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.Size = new System.Drawing.Size(100, 28);
             this.cancelButton.TabIndex = 12;
             this.cancelButton.Text = "Cancel";
             this.cancelButton.UseVisualStyleBackColor = true;
@@ -167,9 +179,10 @@
             // getDatabasesButton
             // 
             this.getDatabasesButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.getDatabasesButton.Location = new System.Drawing.Point(272, 176);
+            this.getDatabasesButton.Location = new System.Drawing.Point(363, 217);
+            this.getDatabasesButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.getDatabasesButton.Name = "getDatabasesButton";
-            this.getDatabasesButton.Size = new System.Drawing.Size(95, 23);
+            this.getDatabasesButton.Size = new System.Drawing.Size(127, 28);
             this.getDatabasesButton.TabIndex = 13;
             this.getDatabasesButton.Text = "Get Databases";
             this.getDatabasesButton.UseVisualStyleBackColor = true;
@@ -178,9 +191,10 @@
             // connectionStringLabel
             // 
             this.connectionStringLabel.AutoSize = true;
-            this.connectionStringLabel.Location = new System.Drawing.Point(22, 256);
+            this.connectionStringLabel.Location = new System.Drawing.Point(29, 315);
+            this.connectionStringLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.connectionStringLabel.Name = "connectionStringLabel";
-            this.connectionStringLabel.Size = new System.Drawing.Size(94, 13);
+            this.connectionStringLabel.Size = new System.Drawing.Size(115, 16);
             this.connectionStringLabel.TabIndex = 15;
             this.connectionStringLabel.Text = "Connection String:";
             // 
@@ -190,17 +204,18 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.connectionStringOutputLabel.AutoEllipsis = true;
             this.connectionStringOutputLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.connectionStringOutputLabel.Location = new System.Drawing.Point(124, 256);
+            this.connectionStringOutputLabel.Location = new System.Drawing.Point(165, 315);
+            this.connectionStringOutputLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.connectionStringOutputLabel.Name = "connectionStringOutputLabel";
-            this.connectionStringOutputLabel.Size = new System.Drawing.Size(243, 13);
+            this.connectionStringOutputLabel.Size = new System.Drawing.Size(324, 16);
             this.connectionStringOutputLabel.TabIndex = 16;
             this.connectionStringOutputLabel.Text = "Connection String Output";
             // 
             // ConnectionStringCreator
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(395, 339);
+            this.ClientSize = new System.Drawing.Size(527, 417);
             this.Controls.Add(this.connectionStringOutputLabel);
             this.Controls.Add(this.connectionStringLabel);
             this.Controls.Add(this.getDatabasesButton);
@@ -216,8 +231,9 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.serverNameLabel);
             this.Controls.Add(this.serverNameTextBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ConnectionStringCreator";
-            this.Text = "Connection String Creator";
+            this.Text = "Add Connection String";
             this.Load += new System.EventHandler(this.ConnectionStringManager_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
